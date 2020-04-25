d53f3380b26c2a7f6eb58316eaca53331f1d9529
Kaska <wlodarczyk@student.agh.edu.pl>
2020-04-25 15:07:20 +0200

    Wydzielony komponent logowania

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,16 @@
           <a @click="logOut()">Wyloguj </a>
       </div>
       <div v-else>
-          <label>Zaloguj się emailem </label>
-          <input type="email" v-model = "email">
-          <button @click="logIn()"> Zaloguj</button>
+          <login-form @login="logIn($event)"></login-form>
       </div>
-
-      <!--<div v-if="email.length < 10">Ale masz krótki adres!</div>
-      <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-      <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-
-      <input type="email" v-model = "email">
-
-      <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>-->
   </div>
 </template>
 
 <script>
 import "milligram";
+import LoginForm from "./LoginForm";
 export default {
+  components: {LoginForm},
   data() {
     return {
       email: '',
@@ -31,8 +23,8 @@ export default {
     };
   },
   methods: {
-      logIn() {
-          this.authenticatedUserName = this.email;
+      logIn(username) {
+          this.authenticatedUserName = username;
         },
     logOut(){
         this.authenticatedUserName = '';
---
+++ src/LoginForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <label>Zaloguj się e-mailem</label>
+        <input type="email" v-model="email">
+        <button @click="enter()">Zaloguj</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                email: ''
+            }
+        },
+        methods: {
+            enter() {
+                this.$emit('login', this.email);
+            }
+        }
+    }
+</script>
